b717dd226b7581a4b126ba4806d0f2f3e192509d
Samuel-Igwudu <igwudu03@gmail.com>
2022-09-14 07:45:20 -0700

    add documentations

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+""" defines a class"""
+
+
 class square:
+    """ class square"""
+
     def __init__(self, size):
+        """ function to initialize.
+
+        Args:
+             size (int): size of the square
+        """
         self.__size = size
