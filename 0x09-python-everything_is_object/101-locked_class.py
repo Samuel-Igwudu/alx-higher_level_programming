09870808055f95e45d444248ca9e24f85c285987
Samuel-Igwudu <igwudu03@gmail.com>
2022-09-20 16:41:04 -0700

    Python - Everything is object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" defines a class
+"""
+
+
+class LockedClass:
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """ method that initialize """
+        pass
+
